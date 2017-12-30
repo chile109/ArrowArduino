ad15c43ec79e43db1b729dbb9149f60ed02d6c25
Kevin Chang <kevin@megalive.com.tw>
2017-12-30 16:34:06 +0800

    [Add] Otter prefab / Animation

---
+++ SaveAnimals/Assets/Script/Animal/ANI_Cry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANI_Cry : BaseState
+{
+    public override void StateDoing(GameObject Obj)
+    {
+        
+    }
+}
