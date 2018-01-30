f1b9a7bdf6996dbee3b0f1ac255839558da5ea34
Kevin Chang <kevin@megalive.com.tw>
2018-01-30 23:08:15 +0800

    [Add] wellcome 場景 加入 video 邏輯

---
+++ SaveAnimals/Assets/Script/wellcome/VideoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour {
+
+    public VideoPlayer _playerA;
+    public VideoPlayer _playerB;
+
+    private void Start()
+    {
+        _playerB.loopPointReached += delegate {
+            SceneManager.LoadSceneAsync("Game");
+        };
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _playerA.Pause();
+            _playerB.gameObject.SetActive(true);
+        }
+    }
+}
